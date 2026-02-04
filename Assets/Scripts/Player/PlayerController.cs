1d320b4d60ab0394ccb4e24846126e69149f03fe
JJH0204 <>
2026-02-04 21:29:06 +0900

    /

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float accel = 1.5f;
+
+    public float jumpPower = 10.0f;
+    public Rigidbody2D rb;
+
+    // 플레이어가 지면에 착지해 있는지 여부를 저장할 변수.
+    bool isGrounded = false;
+
+    float h = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 좌우 방향키나 A, D 키를 눌렀을 때의 입력값을 반환받는다. (-1.0 ~ 1.0)
+        h = Input.GetAxis("Horizontal");
+
+        // GetKey : 키보드의 특정 키가 눌린 상태인지를 체크하는 함수.
+        // GetKeyDown : 키보드의 특정 키가 한 번 눌렸는지를 체크하는 함수.
+        // GetKeyUp : 키보드의 특정 키를 눌렀다가 뗐는지를 체크하는 함수.
+        bool isLeftCtrl = Input.GetKey(KeyCode.LeftControl);
+        if (isLeftCtrl == true)
+        {
+            accel = 1.5f;
+        }
+        else
+        {
+            accel = 1.0f;
+        }
+
+        // 스페이스 키를 누르면 위쪽 방향으로 순간적인 힘을 가한다.
+        // 현재 지면 위에 서 있는지 여부를 같이 체크한다.
+        if (Input.GetKeyDown(KeyCode.Space) == true && isGrounded == true)
+        {
+            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+    }
+
+    // 물리 기반 이동 처리를 할 때 이 함수에서 하는 것이 좋다.
+    void FixedUpdate()
+    {
+        // Rigidbody2D의 속도를 변경해서 오브젝트를 이동시킨다.
+        rb.linearVelocity = new Vector2(h * speed * accel, rb.linearVelocity.y);
+    }
+
+    /// <summary>
+    /// Collider2D가 부착된 오브젝트끼리 충돌했을 때 유니티가 자동으로 호출해주는 함수.
+    /// </summary>
+    /// <param name="collision">충돌한 대상의 각종 정보</param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // CompareTag : 게임 오브젝트의 태그 정보를 비교해주는 함수.
+        if (collision.gameObject.CompareTag("Ground") == true)
+        {
+            isGrounded = true;
+            Debug.Log("땅에 닿았습니다.");
+        }
+    }
+
+    /// <summary>
+    /// 충돌했던 오브젝트가 떨어졌을 때 유니티가 자동으로 호출해주는 함수.
+    /// </summary>
+    /// <param name="collision">떨어진 대상의 각종 정보</param>
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") == true)
+        {
+            isGrounded = false;
+            Debug.Log("땅에서 떨어졌습니다.");
+        }
+    }
+}
