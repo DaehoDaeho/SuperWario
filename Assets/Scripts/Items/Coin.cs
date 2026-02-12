5eb8e339b34bb50800d12ffefa4374af5e318426
JJH0204 <>
2026-02-12 21:28:21 +0900

    .

---
+++ Assets/Scripts/Items/Coin.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    /// <summary>
+    /// Trigger가 켜진 오브젝트와 겹쳤을 때 호출되는 함수.
+    /// </summary>
+    /// <param name="other">충돌한 대상의 정보</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // 충돌한 오브젝트가 플레이어가 맞는지 확인.
+        if (other.CompareTag("Player") == true)
+        {
+            Debug.Log("동전 획득!!!");
+
+            Destroy(gameObject);
+        }
+    }
+}
